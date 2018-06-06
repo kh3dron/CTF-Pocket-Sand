89e820947e0719ef8909d905db5596fd861220f1
Tristan Saldanha <tristansaldanha@gmail.com>
2018-06-06 16:58:45 -0500

    Added Rail Fence
    Added a Rail Fence Cipher method

---
+++ rail_fence.py
@@ -0,0 +1,49 @@
+given = """
+
+ACH! SAVE ME FROM THE WEE TURTLES!
+
+"""
+
+def identifyMessageCase(text):
+
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    check = 0
+    completed = False
+    while not completed:
+        if text[check] in lower:
+            return lower
+        elif text[check] in upper:
+            return upper
+        else:
+            check += 1
+
+clearText = identifyMessageCase(given)
+
+def railFence(text):
+    top, bottom = "", ""    #never knew I could do this
+    tick = 1
+
+    for x in text:
+        if x in clearText:
+            if (tick % 2 == 1):
+                top += x
+            else:
+                bottom += x
+            tick += 1
+
+    return top + bottom
+
+def spaceByFives(text):
+    modified = ""
+    tock = 0
+    for x in text:
+        if ((tock % 5) == 0) & (tock > 1): #skip the first time
+            modified += " "
+        modified += x
+        tock += 1
+    return modified
+
+
+
+print spaceByFives(railFence(given))
